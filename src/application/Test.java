c16ee9cf02ff43a1c808fbca4f7a965d8daf1a0e
StraetenMax <maxstraeten@yahoo.com>
2017-05-22 17:30:19 +0200

    propriétés public

--- src/application/Test.java
+++ src/application/Test.java
@@ -6,6 +6,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		Voiture voiture = new Voiture();
+		Voiture voiture2 = new Voiture(6, "Jaune", "diesel", 75);
+	
+		Voiture voiture3 = new Voiture(4, "Bleu","Essence",170);
+		System.out.println(voiture3.vitesseMax);
+		voiture3.vitesseMax =150;
+		System.out.println(voiture3.vitesseMax);
+		
 	}
 
 }
--- src/vehicule/Voiture.java
+++ src/vehicule/Voiture.java
@@ -2,25 +2,30 @@ package vehicule;
 
 public class Voiture {
 	//propriétés
-	int nbOccupants;
-	String couleur;
-	String energie;
-	int vitesseMax;
+//	private int nbOccupants;
+//	private String couleur;
+//	private String energie;
+//	private int vitesseMax;
+	public int nbOccupants;
+	public String couleur;
+	public String energie;
+	public int vitesseMax;
 	
 	//constructeurs vide et initialisé
 	public Voiture(){
-		System.out.println("Je suis le constructeur vide");
+		System.out.println("La création d'une voiture");
 		nbOccupants = 0;
 		couleur = "";
 		energie = "";
 		vitesseMax = 0;
 	}
-	/*public Voiture(int nbOccupants, String couleur,String energie, int vitesseMax){
+	public Voiture(int nbOccupants, String couleur,String energie, int vitesseMax){
+		System.out.println("la création d'une voiture initialisée");
 		this.nbOccupants = nbOccupants;
 		this.couleur = couleur;
 		this.energie = energie;
 		this.vitesseMax = vitesseMax;
-	}*/
+	}
 	
 
 }
