f84206eabb03caccc173cf7426c8fed06cbf629a
StraetenMax <maxstraeten@yahoo.com>
2017-05-22 17:01:05 +0200

    class test main

---
+++ src/application/Test.java
@@ -0,0 +1,11 @@
+package application;
+
+import vehicule.Voiture;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Voiture voiture = new Voiture();
+	}
+
+}
--- src/vehicule/Voiture.java
+++ src/vehicule/Voiture.java
@@ -1,5 +1,26 @@
 package vehicule;
 
 public class Voiture {
+	//propriétés
+	int nbOccupants;
+	String couleur;
+	String energie;
+	int vitesseMax;
+	
+	//constructeurs vide et initialisé
+	public Voiture(){
+		System.out.println("Je suis le constructeur vide");
+		nbOccupants = 0;
+		couleur = "";
+		energie = "";
+		vitesseMax = 0;
+	}
+	/*public Voiture(int nbOccupants, String couleur,String energie, int vitesseMax){
+		this.nbOccupants = nbOccupants;
+		this.couleur = couleur;
+		this.energie = energie;
+		this.vitesseMax = vitesseMax;
+	}*/
+	
 
 }
