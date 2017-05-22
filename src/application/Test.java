251d01e6fe383a6b767ba0a9e291188b35bf3423
StraetenMax <maxstraeten@yahoo.com>
2017-05-22 18:21:04 +0200

    class Voiture private

--- src/application/Test.java
+++ src/application/Test.java
@@ -9,9 +9,12 @@ public class Test {
 		Voiture voiture2 = new Voiture(6, "Jaune", "diesel", 75);
 	
 		Voiture voiture3 = new Voiture(4, "Bleu","Essence",170);
-		System.out.println(voiture3.vitesseMax);
+		/*System.out.println(voiture3.vitesseMax);
 		voiture3.vitesseMax =150;
-		System.out.println(voiture3.vitesseMax);
+		System.out.println(voiture3.vitesseMax);*/
+		System.out.println(voiture3.getVitesseMax());
+		voiture3.setVitesseMax(150);
+		System.out.println(voiture3.getVitesseMax());
 		
 	}
 
--- src/vehicule/Voiture.java
+++ src/vehicule/Voiture.java
@@ -2,14 +2,15 @@ package vehicule;
 
 public class Voiture {
 	//propriétés
-//	private int nbOccupants;
-//	private String couleur;
-//	private String energie;
-//	private int vitesseMax;
-	public int nbOccupants;
-	public String couleur;
-	public String energie;
-	public int vitesseMax;
+	private int nbOccupants;
+	private String couleur;
+	private String energie;
+	private int vitesseMax;
+	private String typeTransport;
+//	public int nbOccupants;
+//	public String couleur;
+//	public String energie;
+//	public int vitesseMax;
 	
 	//constructeurs vide et initialisé
 	public Voiture(){
@@ -18,6 +19,8 @@ public class Voiture {
 		couleur = "";
 		energie = "";
 		vitesseMax = 0;
+		this.setTypeTransport();
+		
 	}
 	public Voiture(int nbOccupants, String couleur,String energie, int vitesseMax){
 		System.out.println("la création d'une voiture initialisée");
@@ -25,7 +28,44 @@ public class Voiture {
 		this.couleur = couleur;
 		this.energie = energie;
 		this.vitesseMax = vitesseMax;
+		this.setTypeTransport();
 	}
+	
+//	accesseur et mutateur
+		public int getNbOccupants(){
+			return nbOccupants;
+		}
+		public String getCouleur(){
+			return couleur;
+		}
+		public String getEnergie(){
+			return energie;
+		}
+		public int getVitesseMax(){
+			return vitesseMax;
+		}
+		public String getTypeTransport(){
+			return typeTransport;
+		}
+		public void setNbOccupants(int nbOccupants){
+			this.nbOccupants = nbOccupants;
+		}
+		public void setCouleur(String couleur){
+			this.couleur= couleur;
+		}
+		public void setEnergie(String energie){
+			this.energie = energie;
+		}
+		public void setVitesseMax(int vitesseMax){
+			this.vitesseMax=vitesseMax;
+		}
+		public void setTypeTransport(){
+			if(this.nbOccupants < 6){
+				this.typeTransport = "Particulier";
+			}else{
+				this.typeTransport = "En commun";
+			}
+		}
 	
 
 }
