cdeee6c24e3582612a13062f437eea4fec2a30f4
StraetenMax <maxstraeten@yahoo.com>
2017-05-24 10:02:58 +0200

    class VoitureSport constructeur accesseur

--- src/vehicule/VoitureSport.java
+++ src/vehicule/VoitureSport.java
@@ -1,6 +1,25 @@
 package vehicule;
 
 public class VoitureSport extends Voiture {
-	
+	//propriétés
+	private int nbChevaux;
 
+	//constructeurs vide et initialisé
+	public VoitureSport(){
+		super();
+		nbChevaux = 0;
+	}
+	public VoitureSport(int nbOccupants, String couleur,String energie, int vitesseMax, int nbChevaux) throws NbOccupantsException, TypeEnergieException{
+		super(nbOccupants, couleur, energie, vitesseMax);
+		this.nbChevaux = nbChevaux;
+	}
+	
+	//accesseurs et mutateurs
+	public int getNbChevaux(){
+		return nbChevaux;
+	}
+	public void setNbChevaux(int nbChevaux){
+		this.nbChevaux = nbChevaux;
+	}
+	
 }
